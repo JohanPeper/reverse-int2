a3ae23f8c6c4421388a95ced6765e5d8452805de
Ivan <70844716+JohanPeper@users.noreply.github.com>
2023-12-01 11:03:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+    n = String(n);
+    return n = n.split('').reverse().join('');
 }
+
